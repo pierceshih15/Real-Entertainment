283bed8adf14bacb9376ca6157594a244a0f6223
pierce <pierceshih@gmail.com>
2020-04-21 12:06:36 +0800

    Create projectPage draft

---
+++ js/projectPage.js
@@ -0,0 +1,20 @@
+const projects = document.querySelector('#section1 .projects')
+const projectPhoto = projects.querySelectorAll('.project-photo');
+const projectIntro = projects.querySelectorAll('.project-intro');
+
+for (let i = 0; i < projectIntro.length; i++) {
+  projectPhoto[i].addEventListener('mouseover', function () {
+    projectIntro[i].classList.remove('fadeInUp');
+    projectIntro[i].classList.remove('fadeOutDown');
+    projectIntro[i].classList.add('project-intro-hover');
+    projectIntro[i].classList.add('animated', 'fadeInUp');
+  });
+}
+
+for (let i = 0; i < projectIntro.length; i++) {
+  projectPhoto[i].addEventListener('mouseout', function () {
+    projectIntro[i].classList.add('animated', 'fadeOutDown');
+    projectIntro[i].classList.remove('project-intro-hover');
+    projectIntro[i].classList.remove('animated');
+  });
+}
